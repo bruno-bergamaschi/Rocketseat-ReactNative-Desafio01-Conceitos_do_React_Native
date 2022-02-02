c2d1df87579d58536a130c77da8b006ab1ab91f7
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-02-02 09:29:31 -0300

    Conclusão do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,24 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks([...tasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const index = tasks.findIndex(task => task.id === id);
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    updatedTasks[index].done = !updatedTasks[index].done
+    
+    setTasks([...updatedTasks])
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id))
   }
 
   return (
